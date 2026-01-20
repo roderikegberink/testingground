4966af31edd1dcba106a4b6a3ab6c110855de32a
Roderik Egberink <r.egberink@mlgm.nl>
2026-01-20 12:06:11 +0100

    Add functie bij tasks

--- testingdjango/hello/urls.py
+++ testingdjango/hello/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "hello"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:name>", views.greet, name="greet"),
--- testingdjango/newyear/urls.py
+++ testingdjango/newyear/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_names = "newyear"
 urlpatterns = [
-    path("", views.index, name="index.html")
+    path("", views.index, name="index")
 ]
